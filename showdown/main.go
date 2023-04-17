df05564e32adb2a3b33a3f8917f2c932292e580b
mikaeloduh <mikaeloduh@users.noreply.github.com>
2023-04-17 19:07:31 +0800

    refactor: IPlayer

--- showdown/main.go
+++ showdown/main.go
@@ -6,9 +6,10 @@ import (
 )
 
 func main() {
-	p1 := entity.NewHumanPlayer(0, entity.UserInput{})
-	p2 := entity.NewHumanPlayer(1, entity.UserInput{})
-	p3 := entity.NewHumanPlayer(2, entity.UserInput{})
+	userInput := entity.UserInput{}
+	p1 := entity.NewHumanPlayer(0, userInput)
+	p2 := entity.NewHumanPlayer(1, userInput)
+	p3 := entity.NewHumanPlayer(2, userInput)
 	p4 := entity.NewHumanPlayer(3, entity.AIInput{})
 	deck := entity.NewDeck()
 	game := service.NewGame(p1, p2, p3, p4, deck)
