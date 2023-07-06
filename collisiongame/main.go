9ccf154b1144b2a8dad0dfcb10fc7dabd8d4cb76
mikaeloduh <mikaeloduh@users.noreply.github.com>
2023-07-06 17:45:07 +0800

    refactor: use pointer

--- collisiongame/main.go
+++ collisiongame/main.go
@@ -6,6 +6,7 @@ import (
 	"reflect"
 )
 
+// Sprite and its friends
 type Sprite interface {
 	String() string
 }
@@ -29,20 +30,25 @@ func NewHero() *Hero {
 
 type Water struct{}
 
+func NewWater() *Water {
+	return &Water{}
+}
+
 func (w *Water) String() string {
 	return "W"
 }
 
 type Fire struct{}
 
+func NewFire() *Fire {
+	return &Fire{}
+}
+
 func (f *Fire) String() string {
 	return "F"
 }
 
-type World struct {
-	coord [30]Sprite
-}
-
+// TypeSet is set of type
 type TypeSet map[reflect.Type]bool
 
 func NewTypeSet(c1, c2 any) TypeSet {
@@ -53,62 +59,9 @@ func isSameTypeSet(s1, s2 TypeSet) bool {
 	return reflect.DeepEqual(s1, s2)
 }
 
-func (w *World) Move(x1 int, x2 int) {
-	HeroWater := NewTypeSet(new(Hero), new(Water))
-	WaterFire := NewTypeSet(new(Water), new(Fire))
-	FireHero := NewTypeSet(new(Fire), new(Hero))
-
-	c1 := w.coord[x1]
-	c2 := w.coord[x2]
-
-	ss := NewTypeSet(c1, c2)
-
-	// (Hero, Water)
-	if isSameTypeSet(HeroWater, ss) {
-		var handle func(c Sprite, x int)
-		handle = func(c Sprite, x int) {
-			switch t := c.(type) {
-			case *Hero:
-				t.SetHp(10)
-			case *Water:
-				w.coord[x] = nil
-			}
-		}
-
-		handle(c1, x1)
-		handle(c2, x2)
-		w.coord[x1], w.coord[x2] = w.coord[x2], w.coord[x1]
-	} else if isSameTypeSet(WaterFire, ss) {
-		var handle func(c Sprite, x int)
-		handle = func(c Sprite, x int) {
-			switch c.(type) {
-			case *Water:
-				w.coord[x] = nil
-			case *Fire:
-				w.coord[x] = nil
-			}
-		}
-
-		handle(c1, x1)
-		handle(c2, x2)
-		w.coord[x1], w.coord[x2] = w.coord[x2], w.coord[x1]
-	} else if isSameTypeSet(FireHero, ss) {
-		var handle func(c Sprite, x int)
-		handle = func(c Sprite, x int) {
-			switch t := c.(type) {
-			case *Fire:
-				w.coord[x] = nil
-			case *Hero:
-				t.SetHp(-10)
-			}
-		}
-
-		handle(c1, x1)
-		handle(c2, x2)
-		w.coord[x1], w.coord[x2] = w.coord[x2], w.coord[x1]
-	} else if reflect.TypeOf(c1) == reflect.TypeOf(c2) {
-		return
-	}
+// World the sprites' world
+type World struct {
+	coord [30]Sprite
 }
 
 func (w *World) Init() {
@@ -122,8 +75,56 @@ func (w *World) Init() {
 	}
 
 	for _, num := range numbers[:10] {
-		w.coord[num] = NewHero()
+		w.coord[num] = randSprite()
 	}
+}
+
+func randSprite() Sprite {
+	return [3]func() Sprite{
+		func() Sprite { return NewHero() },
+		func() Sprite { return NewWater() },
+		func() Sprite { return NewFire() },
+	}[rand.Intn(3)]()
+}
+
+/**
+ * | Subject \ target |  Hero  | Water  |  Fire  |
+ * | :--------------: | :----: | :----: | :----: |
+ * |       Hero       |  fail  |  +10   |  -10   |
+ * |      Water       | remove |  fail  | remove |
+ * |       Fire       | remove | remove |  fail  |
+ */
+
+func (w *World) Move(x1 int, x2 int) {
+	//isValidMove(c1, c2)
+
+	c1Ptr := &w.coord[x1]
+	c2Ptr := &w.coord[x2]
+
+	//toCollide(c1, c2)
+
+	// toCollide and move
+	if isSameType(*c1Ptr, &Hero{}) && isSameType(*c2Ptr, &Water{}) {
+		// (Hero, Water)
+		(*c1Ptr).(*Hero).SetHp(+10)
+		*c2Ptr = nil
+		*c2Ptr = *c1Ptr
+		*c1Ptr = nil
+	} else if isSameType(*c1Ptr, &Water{}) && isSameType(*c2Ptr, &Fire{}) {
+		// (Water, Fire)
+		*c1Ptr = nil
+		*c2Ptr = nil
+	} else if isSameType(*c1Ptr, &Fire{}) && isSameType(*c2Ptr, &Hero{}) {
+		// (Fire, Hero)
+		*c1Ptr = nil
+		(*c2Ptr).(*Hero).SetHp(-10)
+	} else if isSameType(*c2Ptr, *c2Ptr) {
+		return
+	}
+}
+
+func isSameType(a, b interface{}) bool {
+	return reflect.TypeOf(a) == reflect.TypeOf(b)
 }
 
 func main() {
--- collisiongame/main_test.go
+++ collisiongame/main_test.go
@@ -9,15 +9,14 @@ import (
 
 func Test(t *testing.T) {
 	world := World{}
-
-	assert.IsType(t, World{}, world)
-
 	world.Init()
 
+	assert.IsType(t, World{}, world)
 	assert.Equal(t, 30, len(world.coord))
 }
 
 func TestCollision(t *testing.T) {
+	t.Parallel()
 
 	t.Run("test Hero:Water and Hero +10 and moved, Water be removed", func(t *testing.T) {
 		w := World{
@@ -28,6 +27,7 @@ func TestCollision(t *testing.T) {
 
 		fmt.Printf("%v \n", w.coord[0])
 		fmt.Printf("%v \n", w.coord[1])
+		fmt.Printf("%#v \n", w)
 
 		assert.Equal(t, 30+10, w.coord[1].(*Hero).hp)
 		assert.Equal(t, nil, w.coord[0])
@@ -35,7 +35,7 @@ func TestCollision(t *testing.T) {
 
 	t.Run("test Water:Fire, Water and Fire should be removed", func(t *testing.T) {
 		w := World{
-			coord: [30]Sprite{&Fire{}, &Water{}},
+			coord: [30]Sprite{&Water{}, &Fire{}},
 		}
 
 		w.Move(0, 1)
@@ -49,7 +49,7 @@ func TestCollision(t *testing.T) {
 
 	t.Run("test Fire:Hero, Hero -10 hp and Fire should be removed", func(t *testing.T) {
 		w := World{
-			coord: [30]Sprite{NewHero(), &Fire{}},
+			coord: [30]Sprite{&Fire{}, NewHero()},
 		}
 
 		w.Move(0, 1)
@@ -59,5 +59,14 @@ func TestCollision(t *testing.T) {
 
 		assert.Equal(t, nil, w.coord[0])
 		assert.Equal(t, 30-10, w.coord[1].(*Hero).hp)
+	})
+
+	t.Run("New a world", func(t *testing.T) {
+		w := World{}
+		w.Init()
+
+		fmt.Printf("%#v \n", w)
+
+		assert.Equal(t, 30, len(w.coord))
 	})
 }
