d18568818ef7441aad7ae47c6a2fc717f7965a40
mikaeloduh <mikaeloduh@users.noreply.github.com>
2023-08-21 23:21:11 +0800

    feat: more complete function

--- treasuremap/main.go
+++ treasuremap/main.go
@@ -5,20 +5,57 @@ import (
 	"io"
 )
 
+type AdventureGame struct {
+	round     int
+	character *Character
+}
+
+func NewAdventureGame(character *Character) *AdventureGame {
+	return &AdventureGame{
+		character: character,
+	}
+}
+
+func (g *AdventureGame) StartRound() {
+	g.round++
+	g.character.OnRoundStart()
+}
+
 type Character struct {
 	Writer io.Writer
+	Hp     int
+	State  IState
 }
 
 func NewCharacter(writer io.Writer) *Character {
-	return &Character{Writer: writer}
+	var c *Character
+	c = &Character{Writer: writer, Hp: 300, State: &NormalState{c}}
+	return c
+}
+
+func (c *Character) AddHp(num int) {
+	c.Hp += num
 }
 
 func (c *Character) SetState(s IState) {
+	c.State = s
 	fmt.Fprint(c.Writer, "The character is poisoned.\n")
 }
 
+func (c *Character) OnRoundStart() {
+	c.State.OnRoundStart()
+}
+
+// IState
 type IState interface {
 	OnRoundStart()
+}
+
+type NormalState struct {
+	character *Character
+}
+
+func (s *NormalState) OnRoundStart() {
 }
 
 type PoisonedState struct {
@@ -30,8 +67,7 @@ func NewPoisonedState(character *Character) *PoisonedState {
 }
 
 func (s *PoisonedState) OnRoundStart() {
-	//s.character.AddHp(-15)
-	fmt.Println("Poisoned: -15 HP")
+	s.character.AddHp(-15)
 }
 
 func main() {
--- treasuremap/main_test.go
+++ treasuremap/main_test.go
@@ -8,16 +8,24 @@ import (
 )
 
 func TestCharacterStatus(t *testing.T) {
-	t.Run("happy test", func(t *testing.T) {
-		assert.Equal(t, 2, 1+1)
-	})
 
-	t.Run("test character poisoned and HP -15 on the following two round start", func(t *testing.T) {
+	t.Run("test when character poisoned, HP should -15 for each following two round", func(t *testing.T) {
 		var writer bytes.Buffer
 
 		c := NewCharacter(&writer)
+		g := NewAdventureGame(c)
+
 		c.SetState(NewPoisonedState(c))
 
 		assert.Equal(t, "The character is poisoned.\n", writer.String())
+		assert.Equal(t, 300, c.Hp)
+
+		g.StartRound()
+
+		assert.Equal(t, 300-15, c.Hp)
+
+		g.StartRound()
+
+		assert.Equal(t, 300-15-15, c.Hp)
 	})
 }
