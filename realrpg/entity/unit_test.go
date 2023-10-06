7c515e895c9aeaf9af4f2bf41344f7a9220fa628
mikaeloduh <mikaeloduh@users.noreply.github.com>
2023-10-06 19:40:01 +0800

    refactor: refactor unittest

--- realrpg/entity/unit_test.go
+++ realrpg/entity/unit_test.go
@@ -10,11 +10,12 @@ func TestHero(t *testing.T) {
 	t.Run("test Hero BasicAttack based on it's STR", func(t *testing.T) {
 		unit1 := NewHero()
 		unit2 := NewHero()
+		unit2HP := unit2.HP
 
 		action := unit1.SelectSkill(0)
 		action.SelectTarget([]IUnit{unit2})
 		action.Do()
 
-		assert.Equal(t, 1000-50, unit2.HP)
+		assert.Equal(t, unit2HP-unit1.STR, unit2.HP)
 	})
 }
