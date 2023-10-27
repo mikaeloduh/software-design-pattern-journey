3b90311907894a3401fb646a8b9220f930392f26
mikaeloduh <mikaeloduh@users.noreply.github.com>
2023-10-27 18:03:41 +0800

    feat: add Patient.AddCase

--- prescribersystem/entity/patient.go
+++ prescribersystem/entity/patient.go
@@ -7,15 +7,27 @@ type Case struct {
 }
 
 type Patient struct {
-	Id           string
-	Name         string
-	Gender       Gender
-	Age          int
-	Height       float32
-	Weight       float32
-	PatientCases map[time.Time]Case
+	Id     string
+	Name   string
+	Gender Gender
+	Age    int
+	Height float32
+	Weight float32
+	Cases  map[time.Time]Case
+}
+
+func NewPatient(id string, name string, gender Gender, age int, height float32, weight float32) *Patient {
+	return &Patient{
+		Id:     id,
+		Name:   name,
+		Gender: gender,
+		Age:    age,
+		Height: height,
+		Weight: weight,
+		Cases:  make(map[time.Time]Case),
+	}
 }
 
 func (p *Patient) AddCase(c Case) {
-	p.PatientCases[c.CaseTime] = c
+	p.Cases[c.CaseTime] = c
 }
--- prescribersystem/entity/patient_test.go
+++ prescribersystem/entity/patient_test.go
@@ -1,1 +1,15 @@
 package entity
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPatient_AddCase(t *testing.T) {
+	p := NewPatient("a0000001", "p1", Male, 87, 159, 100)
+	p.AddCase(Case{CaseTime: time.Now()})
+
+	assert.Len(t, p.Cases, 1)
+}
