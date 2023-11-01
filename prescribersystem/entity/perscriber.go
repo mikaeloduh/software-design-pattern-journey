446a1419923beba05bc572f75c2a618b755cbed2
mikaeloduh <mikaeloduh@users.noreply.github.com>
2023-11-01 22:50:41 +0800

    refactor: clean up

--- prescribersystem/entity/perscriber.go
+++ prescribersystem/entity/perscriber.go
@@ -11,12 +11,6 @@ func NewPrescriber(rule IDiagnosticRule) *Prescriber {
 	return &Prescriber{rule: rule}
 }
 
-func NewDefaultPrescriber() *Prescriber {
-	handler := ChainDiagnosticRule(&HerbRule{}, &InhibitorRule{}, &ShutUpRule{})
-
-	return &Prescriber{rule: handler}
-}
-
 func (p *Prescriber) Diagnose(patient Patient, symptoms []Symptom) *Prescription {
 	time.Sleep(3 * time.Second) // Simulate task processing
 
