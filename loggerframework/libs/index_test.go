a888561ee55969bec3a4efca084a117a83248f5e
mikaeloduh <mikaeloduh@users.noreply.github.com>
2024-01-05 19:11:43 +0800

    test: add framework integration test

--- loggerframework/libs/index_test.go
+++ loggerframework/libs/index_test.go
@@ -2,8 +2,9 @@ package libs
 
 import (
 	"bytes"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLoggerFramework(t *testing.T) {
@@ -20,21 +21,48 @@ func TestLoggerFramework(t *testing.T) {
 	})
 }
 
-func TestLoggerFramework_Log(t *testing.T) {
-	t.Run("test GetLoggers should return correct logger and logs message", func(t *testing.T) {
-		var writer bytes.Buffer
+func TestLoggerFramework_IntegrationTest(t *testing.T) {
+	t.Run("test hierarchy logger should output to the correct exporter and logs message", func(t *testing.T) {
+		// io writer for testing
+		var rootWriter bytes.Buffer
+		var gameWriter bytes.Buffer
 
-		root := NewLogger(nil, "root", DEBUG, FakeNewConsoleExporter(&writer), NewStandardLayout())
-		gameLogger := NewLogger(root, "app.game", UNDEFINED, nil, nil)
+		// Temporary file for testing
+		gameLogTempFile := setupFileExporter(t)
+		gameLogBackupTempFile := setupFileExporter(t)
+
+		root := NewLogger(nil, "root", DEBUG, FakeNewConsoleExporter(&rootWriter), NewStandardLayout())
+		gameLogger := NewLogger(root, "app.game", INFO,
+			NewCompositeExporter(
+				FakeNewConsoleExporter(&gameWriter),
+				NewCompositeExporter(
+					NewFileExporter(gameLogTempFile.Name()),
+					NewFileExporter(gameLogBackupTempFile.Name()),
+				),
+			), nil)
+		aiLogger := NewLogger(gameLogger, "app.game.ai", TRACE, nil, nil)
 
 		loggerFramework := NewLoggerFramework()
-		loggerFramework.DeclareLoggers(root, gameLogger)
+		loggerFramework.DeclareLoggers(root, gameLogger, aiLogger)
 
+		// Test app.game
 		appGameLogger := loggerFramework.GetLoggers("app.game")
+		appGameLogger.Info("log for game")
 
-		appGameLogger.Debug("log for game")
+		// assert app.game console exporter
+		assert.Regexp(t, `\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3} \|-INFO app.game - log for game`, gameWriter.String())
+		gameWriter.Reset()
 
-		assert.Regexp(t, `\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3} \|-DEBUG app.game - log for game`, writer.String())
-		writer.Reset()
+		// assert app.game file exporter
+		assertFileContent(t, gameLogTempFile, `\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3} \|-INFO app.game - log for game`)
+		assertFileContent(t, gameLogBackupTempFile, `\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3} \|-INFO app.game - log for game`)
+
+		// Test app.game.ai
+		appGameAiLogger := loggerFramework.GetLoggers("app.game.ai")
+		appGameAiLogger.Trace("log for ai")
+
+		// assert app.game console exporter
+		assert.Regexp(t, `\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3} \|-TRACE app.game.ai - log for ai`, gameWriter.String())
+		gameWriter.Reset()
 	})
 }
