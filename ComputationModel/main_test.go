b415901a60ca527bd4353e0afdca4548937e5688
mikaeloduh <mikaeloduh@users.noreply.github.com>
2024-09-12 10:05:08 +0800

    test: refine test name

--- ComputationModel/main_test.go
+++ ComputationModel/main_test.go
@@ -11,11 +11,11 @@ func Test_Main(t *testing.T) {
 
 	assert.IsType(t, &Models{}, models)
 
-	t.Run("test scaling model", func(t *testing.T) {
+	t.Run("Creating Scaling model should return correct transform matrix", func(t *testing.T) {
 		scalingModel, err := models.CreateModel("Scaling")
 		assert.NoError(t, err)
 
-		array := testArray(1000)
+		array := testArray(1.0, 1000)
 
 		output, err2 := scalingModel.LinearTransformation(array)
 		assert.NoError(t, err2)
@@ -28,30 +28,28 @@ func Test_Main(t *testing.T) {
 
 	var scalingModel IModel
 
-	t.Run("Validating array length must equal the model's row size ", func(t *testing.T) {
+	t.Run("Validating array length must equal the model's row size", func(t *testing.T) {
 		var err error
 		scalingModel, err = models.CreateModel("Scaling")
 		assert.NoError(t, err)
-
-		array := testArray(999)
+		array := testArray(1.0, 999)
 
 		_, err2 := scalingModel.LinearTransformation(array)
 
 		assert.Error(t, err2)
 	})
 
-	t.Run("Calling CreateModel multiple times should return the same instance", func(t *testing.T) {
+	t.Run("Calling CreateModel multiple times should always return the same instance", func(t *testing.T) {
 		testScalingModel, err := models.CreateModel("Scaling")
 		assert.NoError(t, err)
 
 		assert.Same(t, scalingModel, testScalingModel)
 	})
 
-	t.Run("test Reflection model", func(t *testing.T) {
+	t.Run("Creating Reflection model should return correct transform matrix", func(t *testing.T) {
 		reflectionModel, err := models.CreateModel("Reflection")
 		assert.NoError(t, err)
-
-		array := testArray(1000)
+		array := testArray(1.0, 1000)
 
 		output, err2 := reflectionModel.LinearTransformation(array)
 		assert.NoError(t, err2)
@@ -64,12 +62,12 @@ func Test_Main(t *testing.T) {
 }
 
 // helpers
-func testArray(size int) []float64 {
+func testArray(value float64, size int) []float64 {
 	//  creates an array of 1000 floats, all filled with the value 1.0
 	arr := make([]float64, size)
 
 	for i := 0; i < len(arr); i++ {
-		arr[i] = 1.0
+		arr[i] = value
 	}
 
 	return arr
